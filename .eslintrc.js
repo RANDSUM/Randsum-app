964eafdf1d7f37553afdda24ae249a6a43479bb8
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-04-28 06:33:40 -0400

    idk man

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['expo', 'prettier'],
   ignorePatterns: ['/dist/*'],
   plugins: ['prettier'],
+  settings: {
+    'import/ignore': ['react-native']
+  },
   rules: {
     'prettier/prettier': 'error'
   }
